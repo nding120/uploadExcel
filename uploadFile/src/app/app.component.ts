814608620483f8e2200750a57a86d52698936cce
Daisssy <nding120@gmail.com>
2019-10-22 14:55:17 -0400

    upload file

--- uploadFile/src/app/app.component.ts
+++ uploadFile/src/app/app.component.ts
@@ -33,7 +33,6 @@ export class AppComponent {
     var files = e.target.files; // 
     var i, f;
     var newArry = [];//
-    var newobj = [];//
     for (i = 0, f = files[i]; i != files.length; ++i) { // 
       var reader = new FileReader(); // 
       var name = f.name;
@@ -45,27 +44,62 @@ export class AppComponent {
 
         /* DO SOMETHING WITH workbook HERE */
         var sheet_name_list = workbook.SheetNames;
+        console.log(sheet_name_list);
         sheet_name_list.forEach(function (y) { /* iterate through sheets */
           var worksheet = workbook.Sheets[y];
-          console.log(y);
-          XLSX.utils.sheet_to_json(worksheet);
-          console.log(worksheet);
+          console.log(y); // SHEET1
+          // XLSX.utils.sheet_to_json(worksheet);
+          console.log(worksheet); // WHOLE CONTENT
+          var item;
+          var beginLine;
           for (let z in worksheet) {
+            var colNum;
+            var rowNum;
             /* all keys that do not begin with "!" correspond to cell addresses */
-            console.log(z); // A1
-            // if (z === "!ref" || z === "A1" || z === "B1" || z === "C1" || z === "D1") continue;
-            // for (let i = 0; i < 4; i++) {
-            // newobj.push(JSON.stringify(worksheet[z].v));
-            // }
-            // newArry.push(newobj);
+            // console.log(z); // A1
             if (z[0] === '!') {
-              console.log(z[0]); continue;
             }
-            newArry.push(worksheet[z].v);
+            if (z === '!ref') {
+              item = z;
+              console.log(worksheet[z]);// A1:D4
+              var wholeLength = worksheet[z].split(":");
+              let startAlpha = wholeLength[0][0];
+              let startNum = +wholeLength[0].substring(1);
+              beginLine = startNum + 1;
+              let endAlpha = wholeLength[1][0];
+              let endNum = +wholeLength[1].substring(1);
+              colNum = endAlpha.charCodeAt(0) - startAlpha.charCodeAt(0) + 1;
+              rowNum = endNum - startNum;
+              console.log(colNum, rowNum);
+            }
+
+            // newArry.push(worksheet[z].v);
             // console.log(y + "!" + z + "=" + JSON.stringify(worksheet[z].v));
+          }
+
+          // beginLine = +(worksheet[item][1])+1;
+          for (let j = beginLine; j < rowNum + beginLine; j++) {
+            let newobj = [];
+            for (let i = 0; i < colNum; i++) {
+              var a = worksheet[item].charCodeAt(0);
+              var b = String.fromCharCode(a + i);
+              let newItem = b + j;
+              // console.log(b, newItem);
+              if (worksheet[newItem]===undefined) {
+                newobj.push('');
+              }
+              else {
+                newobj.push(worksheet[newItem].v);
+                console.log(worksheet[newItem].v);
+              }
+              
+            }
+            console.log(newobj);
+            newArry.push(newobj); //newArry.push([...newobj]); if {}, newArry.push({...newobj});
+            console.log(newArry);
 
           }
-          console.log(newArry);
+
         });
 
 
